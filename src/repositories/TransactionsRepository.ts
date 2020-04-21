a9470189b04563d0f79f30f87902eef021dba3e8
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-04-21 20:20:47 -0300

    teste finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let incomeValues = 0;
+    let outcomeValues = 0;
+
+    this.transactions.forEach(transaction => {
+      if (transaction.type === 'income') {
+        incomeValues += transaction.value;
+      } else if (transaction.type === 'outcome') {
+        outcomeValues += transaction.value;
+      }
+    });
+
+    const balance = {
+      income: incomeValues,
+      outcome: outcomeValues,
+      total: incomeValues - outcomeValues,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
